0e5b8feaab039ceeb3f5689806e86f7a0d5a8df1
perryhowe <perry.howe@gmail.com>
2026-03-17 20:28:34 +0000

    feat: add delta time and frame rate limiting

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -17,8 +19,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-
-
+        dt = (game_clock.tick(60)) / 1000
 
 if __name__ == "__main__":
     main()

9f8598aa297b133f35536f8fa44b237b062e2f42
perryhowe <perry.howe@gmail.com>
2026-03-21 20:04:38 +0000

    feat: add asteroid split on kill

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,9 @@
 from circleshape import CircleShape
 import pygame
-from constants import LINE_WIDTH
+from constants import *
+from logger import log_event
+import random
+
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +14,22 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += (self.velocity * dt) 
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        random_angle = random.uniform(20, 50)
+        new_vector = self.velocity.rotate(random_angle)
+        new_vector2 = self.velocity.rotate(-random_angle)
+        new_size = self.radius - ASTEROID_MIN_RADIUS
+        new_asteroid = Asteroid(self.position, self.position, new_size)
+        new_asteroid2 = Asteroid(self.position, self.position, new_size)
+        new_asteroid.velocity = new_vector * 1.2
+        new_asteroid2.velocity = new_vector2 * 1.2
+
+
+
+
+        
